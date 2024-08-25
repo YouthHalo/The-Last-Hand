a0587810109aa5b1a9cda8d2413d4c441712deff
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-25 14:18:23 -0400

    renaming cards to values

--- scripts/card.gd
+++ scripts/card.gd
@@ -4,12 +4,13 @@ var num = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if num < 10:
-		$Sprite2D.texture = load("res://assets/cards/sprite_0" + str(num) + ".png")
+		$Sprite2D.texture = load("res://assets/cards/value/sprite_0" + str(num) + ".png")
 	else:
-		$Sprite2D.texture = load("res://assets/cards/sprite_" + str(num) + ".png")
+		$Sprite2D.texture = load("res://assets/cards/value/sprite_" + str(num) + ".png")
 	print(num)
 	position = Vector2(-100-num*2, 0)
 	print(position)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/deck.gd
+++ scripts/deck.gd
@@ -12,6 +12,8 @@ var deckArray = [
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		var cardInstance= card.instantiate()
@@ -19,6 +21,23 @@ func _input(event: InputEvent) -> void:
 		cardInstance.num = currentcard
 		add_child(cardInstance)
 		print(deckArray.size())
+
+
+func card_value(card: String) -> int:
+	var rank = card[0]
+	var suit = card[1]
+	
+	# Define the order for ranks
+	var rank_order = {"2": 0, "3": 1, "4": 2, "5": 3, "6": 4, "7": 5, "8": 6, "9": 7, "T": 8, "J": 9, "Q": 10, "K": 11, "A": 12}
+	# Define the order for suits (Hearts, Diamonds, Spades, Clubs)
+	var suit_order = {"H": 0, "D": 1, "S": 2, "C": 3}
+	
+	return rank_order[rank] + suit_order[suit] * 13
+
+
+func custom_sort(a: String, b: String) -> bool:
+	return card_value(a) < card_value(b)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
