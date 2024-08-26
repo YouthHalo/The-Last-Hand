fd933cb9ca7644e93e9c66afedaff844c0bd4c15
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-26 13:51:10 -0400

    More Enemy stuff

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,33 @@
 extends Node2D
 
+var floatUp = true
 
+var health = 50.0
+var shield = 0.0
+var dealDamage = 0
+var stunChance = 0
+var bleedChance = 0
+var deckArray = [
+	"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH", "AH",
+	"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD", "AD",
+	"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS", "AS",
+	"2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC", "AC"
+];
+var currentcards = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in 3:
+		currentcards.append(deckArray[randi_range(0, 51)])
+		print(currentcards)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if floatUp:
+		position = position.lerp(Vector2(576, 150), delta * 3)
+		if position.y < 150.5:
+			floatUp = false
+	else:
+		position = position.lerp(Vector2(576, 200), delta * 3)
+		if position.y > 199.5:
+			floatUp = true
